b2fcaf0d923edde43ab3146ae1137d222f3b57c6
HotelWorks <hotelhotel.kr@gmail.com>
2026-01-22 00:28:18 +0900

    chore: MySQL 愿???뚯씪 諛??섏〈???쒓굅 - Supabase ?꾪솚 ?꾨즺

--- create-database.sql
+++
@@ -1,6 +0,0 @@
--- HotelWorks 데이터베이스 생성
-CREATE DATABASE IF NOT EXISTS hotelworks CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE hotelworks;
-
--- 확인 메시지
-SELECT 'Database hotelworks created successfully!' AS message;
